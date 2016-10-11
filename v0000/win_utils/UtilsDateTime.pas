a0a7723f030cde14cbdf1660a7381a74a8eab335
litwrd <litwrd@github.com>
2016-10-11 11:36:19 +0800

    update utils date time parser

--- v0000/win_utils/UtilsDateTime.pas
+++ v0000/win_utils/UtilsDateTime.pas
@@ -46,30 +46,67 @@ var
   i: integer;
   tmpStrDate: AnsiString;
   tmpStrTime: AnsiString;
-  tmpStrHead: AnsiString;
-  tmpStrEnd: AnsiString;
   tmpFormat: TFormatSettings;
 begin
   // 2014/06/12-10:30
-  i := Pos('-', ADateTimeString);
+  i := 0;       
+  if 0 < Pos(':', ADateTimeString) then
+  begin
+    tmpFormat.TimeSeparator := ':';
+    i := Pos('-', ADateTimeString);   
+    if 0 >= i then
+      i := Pos(#32, ADateTimeString);
+  end;
+  FillChar(tmpFormat, SizeOf(tmpFormat), 0);
   if 0 < i then
   begin
     tmpStrDate := Copy(ADateTimeString, 1, i - 1);
-    tmpStrTime := Copy(ADateTimeString, i + 1, maxint);
-    FillChar(tmpFormat, SizeOf(tmpFormat), 0);
-    tmpFormat.DateSeparator := '/';
-    tmpFormat.TimeSeparator := ':'; 
-    tmpFormat.ShortDateFormat := 'yyyy/mm/dd';
-    tmpFormat.LongDateFormat := 'yyyy/mm/dd';
-    tmpFormat.ShortTimeFormat := 'hh:nn:ss';
-    tmpFormat.LongTimeFormat := 'hh:nn:ss';
-    ADateTimeParse.Date := StrToDateDef(tmpStrDate, 0, tmpFormat);
-    ADateTimeParse.Time := StrToTimeDef(tmpStrTime, 0, tmpFormat);
-    if 0 < ADateTimeParse.Date then
-      DecodeDate(ADateTimeParse.Date, ADateTimeParse.Year, ADateTimeParse.Month, ADateTimeParse.Day);
-    if 0 < ADateTimeParse.Time then
-      DecodeTime(ADateTimeParse.Time, ADateTimeParse.Hour, ADateTimeParse.Minute, ADateTimeParse.Second, ADateTimeParse.MSecond);
+    tmpStrTime := Copy(ADateTimeString, i + 1, maxint);  
+  end else
+  begin
+    tmpStrDate := ADateTimeString;
+    tmpStrTime := '';
   end;
+  if 0 < Pos('/', tmpStrDate) then
+  begin
+    tmpFormat.DateSeparator := '/';
+  end else
+  begin
+    if 0 < Pos('-', tmpStrDate) then
+      tmpFormat.DateSeparator := '-';
+  end;
+  tmpFormat.ShortDateFormat := 'yyyy' + tmpFormat.DateSeparator + 'mm' + tmpFormat.DateSeparator + 'dd';
+  tmpFormat.LongDateFormat := 'yyyy' + tmpFormat.DateSeparator + 'mm' + tmpFormat.DateSeparator + 'dd';
+  tmpFormat.ShortTimeFormat := 'hh:nn:ss';
+  tmpFormat.LongTimeFormat := 'hh:nn:ss';
+  ADateTimeParse.Date := StrToDateDef(tmpStrDate, 0, tmpFormat);
+  if '' <> tmpStrTime then
+  begin
+    ADateTimeParse.Time := StrToTimeDef(tmpStrTime, 0, tmpFormat);
+    if 0 = ADateTimeParse.Time then
+    begin
+      // 11:30
+      i := Pos(':', tmpStrTime);
+      if 0 < i then
+      begin
+        ADateTimeParse.Hour := StrToIntDef(Copy(tmpStrTime, 1, i - 1), 0);
+        tmpStrTime := Copy(tmpStrTime, i + 1, maxint);
+        i := Pos(':', tmpStrTime);
+        if 0 < i then
+        begin
+
+        end else
+        begin
+          ADateTimeParse.Minute := StrToIntDef(tmpStrTime, 0);
+          ADateTimeParse.Time := EncodeTime(ADateTimeParse.Hour, ADateTimeParse.Minute, 0, 0);
+        end;
+      end;
+    end;
+  end;
+  if 0 < ADateTimeParse.Date then
+    DecodeDate(ADateTimeParse.Date, ADateTimeParse.Year, ADateTimeParse.Month, ADateTimeParse.Day);
+  if 0 < ADateTimeParse.Time then
+    DecodeTime(ADateTimeParse.Time, ADateTimeParse.Hour, ADateTimeParse.Minute, ADateTimeParse.Second, ADateTimeParse.MSecond);
 end;
 
 end.
