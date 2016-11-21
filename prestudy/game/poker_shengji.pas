52bdec9ae26d7325b6e90a6393281d8fac084d56
litwrd <litwrd@github.com>
2016-11-21 10:26:44 +0800

    add shengji

---
+++ prestudy/game/poker_shengji.pas
@@ -0,0 +1,22 @@
+unit poker_shengji;
+
+interface
+
+(*
+    升级 80分 拖拉机
+    // 1. shuffle
+    // 2. Dealing, Deing, Licensing
+    //    2.1 发牌
+    //    2.2 叫牌 [叫主] 抢庄 hog [ 底牌+6 起始出牌位置 ]
+    // 3. 埋底 []
+    // 4. 出牌  回合 round
+             规则
+                 主牌
+                 副牌
+                 连对 牌型匹配
+    // 5. 结算
+    //       底牌分结算
+*)
+implementation
+
+end.
