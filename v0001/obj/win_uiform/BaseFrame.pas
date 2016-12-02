f054ce71860f938fe06463ec4af48e483b2d2c6e
litwrd <litwrd@github.com>
2016-12-02 17:29:09 +0800

    update base frame

--- v0001/obj/win_uiform/BaseFrame.pas
+++ v0001/obj/win_uiform/BaseFrame.pas
@@ -16,6 +16,9 @@ type
     fBaseFrameData: TfmeBaseData;
   public
     constructor Create(AOwner: TComponent); override;
+    procedure Initialize; virtual;
+    procedure CallDeactivate; virtual;
+    procedure CallActivate; virtual;
     property App: TBaseApp read fBaseFrameData.App write fBaseFrameData.App;  
   end;
 
@@ -29,6 +32,18 @@ constructor TfmeBase.Create(AOwner: TComponent);
 begin
   inherited;
   FillChar(fBaseFrameData, SizeOf(fBaseFrameData), 0);
+end;
+
+procedure TfmeBase.Initialize;
+begin
+end;
+
+procedure TfmeBase.CallDeactivate;
+begin
+end;
+
+procedure TfmeBase.CallActivate; 
+begin
 end;
 
 end.
