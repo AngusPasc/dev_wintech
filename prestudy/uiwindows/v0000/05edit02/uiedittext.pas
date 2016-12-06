98b8102d484845b26d74c4db13b64fdce3857930
litwrd <litwrd@github.com>
2016-12-06 15:58:05 +0800

    update ui edit text

--- prestudy/uiwindows/v0000/05edit02/uiedittext.pas
+++ prestudy/uiwindows/v0000/05edit02/uiedittext.pas
@@ -2,6 +2,10 @@ unit uiedittext;
 
 interface
 
+(*
+  // all this unit should not use any windows unit
+  // it should be independence of platform 
+*)
 type
   PUITextDataNodeW  = ^TUITextDataNodeW;    
   PUITextLine       = ^TUITextLine;   
@@ -127,6 +131,8 @@ begin
 end;
              
 procedure EditTextAdd(AEditText: PUIEditText; AChar: WideChar);
+var
+  tmpNode: PUITextDataNodeW;
 begin    
   if nil = AEditText then
     exit;
@@ -140,9 +146,40 @@ begin
   end;
   if AEditText.EditPos.EditDataNode.Size = AEditText.EditPos.EditDataNode.Length then
   begin
-    AEditText.EditPos.EditDataNode := CheckOutTextDataNode;  
-    InsertTextDataNode(AEditText.EditLine, AEditText.EditPos.EditDataNode, nil);
-    AEditText.EditPos.NodePos := 0;
+    tmpNode := CheckOutTextDataNode;
+    if AEditText.EditPos.NodePos = AEditText.EditPos.EditDataNode.Size then
+    begin
+      // position is last
+      InsertTextDataNode(AEditText.EditLine, tmpNode, AEditText.EditPos.EditDataNode.NextSibling);
+      AEditText.EditPos.EditDataNode := tmpNode;
+      AEditText.EditPos.NodePos := 0;
+    end else
+    begin
+      if 0 = AEditText.EditPos.NodePos then
+      begin
+        // position is head  
+        InsertTextDataNode(AEditText.EditLine, tmpNode, AEditText.EditPos.EditDataNode);
+        AEditText.EditPos.EditDataNode := tmpNode;
+        AEditText.EditPos.NodePos := 0;
+      end else
+      begin         
+        // position is middle always insert after position
+        // [ DataA Position DataB]
+        //
+        InsertTextDataNode(AEditText.EditLine, tmpNode, AEditText.EditPos.EditDataNode.NextSibling);
+        // move DataB to new data buffer
+        //CopyMemory
+        Move(
+          AEditText.EditPos.EditDataNode.Data[AEditText.EditPos.NodePos],
+          tmpNode.Data[0],
+          (AEditText.EditPos.EditDataNode.Size - AEditText.EditPos.NodePos)
+        );
+        // relength DataA Buffer
+        AEditText.EditPos.EditDataNode.Length := AEditText.EditPos.NodePos;
+        // relength new data Node Buffer
+        tmpNode.Length := AEditText.EditPos.EditDataNode.Length - AEditText.EditPos.NodePos;
+      end;
+    end;
   end;
   if nil <> AEditText.EditPos.EditDataNode then
   begin
@@ -156,10 +193,19 @@ end;
 
 procedure EditTextDelete(AEditText: PUIEditText);
 begin
+  if 0 < AEditText.EditPos.NodePos  then
+  begin
+    AEditText.EditPos.EditDataNode.Length := AEditText.EditPos.EditDataNode.Length - 1;
+  end;
 end;
 
 procedure EditTextBackspace(AEditText: PUIEditText);
-begin
+begin 
+  if 0 < AEditText.EditPos.NodePos  then
+  begin
+    // move data 
+    AEditText.EditPos.EditDataNode.Length := AEditText.EditPos.EditDataNode.Length - 1;
+  end;
 end;
 
 end.
